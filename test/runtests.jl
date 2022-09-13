13e5b88a63bf1069f7fc89d5c8deb6d2e3d21b9b
lukketotte <lukas.arnroth@gmail.com>
2022-09-13 15:56:25 +0200

    added test

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,7 @@ using Distributions, DataFrames, StatsModels, LinearAlgebra, MCMCChains
     f1 = @formula(x1 ~ 1 + a + x3 + x4)
     f = @formula(x1 ~ 1 + x2 + x3 + x4)
 
-    #@test_throws ArgumentError bqr(f1, df, 0.5, 100, 1)
+    @test_throws ArgumentError bqr(f1, df, 0.5, 100, 1)
     @test_throws DimensionMismatch bqr(y, X[1:(n-2),:], 0.5, 100, 1)
     @test_throws DomainError bqr(y, X, -0.1, 100, 1)
     @test_throws DomainError bqr(f, df, -0.1, 100, 1)
